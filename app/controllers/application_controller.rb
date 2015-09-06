bc89e59537891f3941bdf13f1717b26b5bbdf01c
Cameron Kidman <cameronjkidman@gmail.com>
2015-09-06 14:10:26 -0600

    Get application to always render JSON and configure pow to detect proper settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::API
   include ActionController::Serialization
   include ActionController::ImplicitRender
 
+  before_action :set_default_response_format
+
+  protected
+
+  def set_default_response_format
+    request.format = :json
+  end
+
 end
