84ca55e5b795e2aca4ee15a90b02a795f96f4c9a
Cameron Kidman <cameronjkidman@gmail.com>
2015-09-08 19:55:09 -0600

    Add update action to users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for(:user)
-        post :create, { "user" => @user_attributes }, format: :json
+        post :create, { user: @user_attributes }, format: :json
       end
 
       it "renders the JSON for the user we just created" do
@@ -53,6 +53,42 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
 
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }, format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:user][:email]).to eql "newmail@example.com"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id, user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on whye the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { should respond_with 422 }
     end
   end
 end
