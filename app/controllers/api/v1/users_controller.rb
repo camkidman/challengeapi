41c1af88433b96b79df359c697782b2723317bda
Cameron Kidman <cameronjkidman@gmail.com>
2015-10-16 21:29:09 -0600

    Remove respond_with

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::UsersController < ApplicationController
   respond_to :json
 
   def show
-    respond_with User.find(params[:id])
+    user = User.find(params[:id])
+    render json: user, status: 200, location: [:api, user]
   end
 
   def create
